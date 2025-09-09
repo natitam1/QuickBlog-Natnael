a4c6173fceb18f2ac369b4cbdebe2a73834f17c2
Natnael Tamiru <natnaeltamiru98@gmail.com>
2025-09-09 13:33:49 +0300

    Set up pages route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,17 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Blog from "./pages/Blog";
 
 const App = () => {
-  return <div className="text-5xl">App</div>;
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/blog" element={<Blog />} />
+      </Routes>
+    </div>
+  );
 };
 
 export default App;
