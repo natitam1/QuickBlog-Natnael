71a25f48109b49dabfc9594dcca44b1536fd6d7e
Natnael Tamiru <natnaeltamiru98@gmail.com>
2025-09-10 07:58:40 +0300

    Add motion

--- client/src/components/BlogList.jsx
+++ client/src/components/BlogList.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { blogCategories } from "../assets/assets";
+import { motion } from "motion/react";
 
 const BlogList = () => {
   const [menu, setMenu] = useState("All");
@@ -16,7 +17,11 @@ const BlogList = () => {
             >
               {item}
               {menu === item && (
-                <div className="absolute left-0 right-0 top-0 h-7 -z-1 bg-primary rounded-full"></div>
+                <motion.div
+                  layoutId="underline"
+                  transition={{ type: "spring", stiffness: 500, damping: 30 }}
+                  className="absolute left-0 right-0 top-0 h-7 -z-1 bg-primary rounded-full"
+                ></motion.div>
               )}
             </button>
           </div>
