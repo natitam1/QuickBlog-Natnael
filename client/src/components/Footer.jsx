45d36ff3023ea5c0bd205ec3447d2b577996e0ab
Natnael Tamiru <natnaeltamiru98@gmail.com>
2025-09-10 12:38:06 +0300

    Add footer section

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,69 @@
+import { assets } from "../assets/assets";
+
+const Footer = () => {
+  const linkSections = [
+    {
+      title: "Quick Links",
+      links: ["Home", "Best Sellers", "Offers & Deals", "Contact Us", "FAQs"],
+    },
+    {
+      title: "Need Help?",
+      links: [
+        "Delivery Information",
+        "Return & Refund Policy",
+        "Payment Methods",
+        "Track your Order",
+        "Contact Us",
+      ],
+    },
+    {
+      title: "Follow Us",
+      links: ["Instagram", "Twitter", "Facebook", "YouTube"],
+    },
+  ];
+
+  return (
+    <div className="px-6 md:px-16 lg:px-24 xl:px-32">
+      <div className="flex flex-col md:flex-row items-start justify-between gap-10 py-10 border-b border-gray-500/30 text-gray-500">
+        <div>
+          <img className="w-32 sm:w-44" src={assets.logo} alt="logo" />
+          <p className="max-w-[410px] mt-6">
+            We build cutting-edge AI solutions that help businesses work smarter
+            and faster. Our mission is to make advanced technology accessible,
+            reliable, and impactful.
+          </p>
+        </div>
+        <div className="flex flex-wrap justify-between w-full md:w-[45%] gap-5">
+          {linkSections.map((section, index) => (
+            <div key={index}>
+              <h3 className="font-semibold text-base text-gray-900 md:mb-5 mb-2">
+                {section.title}
+              </h3>
+              <ul className="text-sm space-y-1">
+                {section.links.map((link, i) => (
+                  <li key={i}>
+                    <a href="#" className="hover:underline transition">
+                      {link}
+                    </a>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          ))}
+        </div>
+      </div>
+      <p className="py-4 text-center text-sm md:text-base text-gray-500/80">
+        Copyright 2025 ©{" "}
+        <a
+          href="https://www.linkedin.com/in/natitam1/"
+          className="text-primary hover:text-gray-500/80"
+        >
+          Natnael Tamiru
+        </a>{" "}
+        All Right Reserved.
+      </p>
+    </div>
+  );
+};
+
+export default Footer;
