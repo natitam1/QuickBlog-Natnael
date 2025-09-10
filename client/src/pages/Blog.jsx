08e95ec2b8a6a4bee0cbdc696ad410cd1504835c
Natnael Tamiru <natnaeltamiru98@gmail.com>
2025-09-10 16:37:14 +0300

    Create a detailed page for blog

--- client/src/pages/Blog.jsx
+++ client/src/pages/Blog.jsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import { blog_data } from "../assets/assets";
+import { assets, blog_data } from "../assets/assets";
+import Navbar from "../components/Navbar";
+import Moment from "moment";
 
 const Blog = () => {
   const { id } = useParams();
@@ -14,7 +16,37 @@ const Blog = () => {
   useEffect(() => {
     fetchBlogData();
   }, []);
-  return <div></div>;
+  return data ? (
+    <div className="relative">
+      <img
+        src={assets.gradientBackground}
+        className="absolute -top-50 -z-1 opacity-50"
+        alt=""
+      />
+      <Navbar />
+      <div className="text-center mt-20 text-gray-600">
+        <p className="text-primary py-4 font-medium">
+          Published on {Moment(data.createdAt).format("MMMM Do YYYY")}
+        </p>
+        <h1 className="text-2xl sm:text-5xl font-semibold max-w-2xl mx-auto text-gray-800">
+          {data.title}
+        </h1>
+        <h2 className="my-5 max-w-lg truncate mx-auto">{data.subTitle}</h2>
+        <p className="inline-block py-1 px-4 rounded-full mb-6 border text-sm border-primary/35 bg-primary/5 font-medium text-primary">
+          Michael Brown
+        </p>
+      </div>
+      <div className="mx-5 max-w-5xl md:mx-auto my-10 mt-6">
+        <img src={data.image} className="rounded-3xl mb-5" alt="" />
+        <div
+          className="rich-text max-w-3xl mx-auto"
+          dangerouslySetInnerHTML={{ __html: data.description }}
+        ></div>
+      </div>
+    </div>
+  ) : (
+    <div>Loading</div>
+  );
 };
 
 export default Blog;
