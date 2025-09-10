c2e7753d0ce2551d3a00d7a9cbb577ec101a547a
Natnael Tamiru <natnaeltamiru98@gmail.com>
2025-09-10 13:11:29 +0300

    Add state variables and set the logic behind it

--- client/src/pages/Blog.jsx
+++ client/src/pages/Blog.jsx
@@ -1,7 +1,20 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { blog_data } from "../assets/assets";
 
 const Blog = () => {
-  return <div>Blog</div>;
+  const { id } = useParams();
+  const [data, setData] = useState(null);
+
+  const fetchBlogData = async () => {
+    const data = blog_data.find((item) => item._id === id);
+    setData(data);
+  };
+
+  useEffect(() => {
+    fetchBlogData();
+  }, []);
+  return <div></div>;
 };
 
 export default Blog;
