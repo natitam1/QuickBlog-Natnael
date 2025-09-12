e739bb5ca46f9d298b196d44486123a62246cab9
Natnael Tamiru <natnaeltamiru98@gmail.com>
2025-09-12 19:01:37 +0300

    Add a navigation to the admin dashboard

--- client/src/pages/admin/Layout.jsx
+++ client/src/pages/admin/Layout.jsx
@@ -1,7 +1,24 @@
 import React from "react";
+import { assets } from "../../assets/assets";
+import { useNavigate } from "react-router-dom";
 
 const Layout = () => {
-  return <div>Layout</div>;
+  const navigate = useNavigate();
+  return (
+    <>
+      <div className="flex items-center justify-between py-2 h-[70px] px-4 sm:px-12 border-b border-gray-200">
+        <img
+          src={assets.logo}
+          onClick={() => navigate("/")}
+          alt=""
+          className="w-32 sm:w-40 cursor-pointer"
+        />
+        <button className="text-sm px-8 py-2 bg-primary text-white rounded-full cursor-pointer">
+          Logout
+        </button>
+      </div>
+    </>
+  );
 };
 
 export default Layout;
