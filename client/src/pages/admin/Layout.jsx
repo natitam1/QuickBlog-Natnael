c289d09d90913ec49054643a3005121fd385b3a5
Natnael Tamiru <natnaeltamiru98@gmail.com>
2025-09-12 19:21:41 +0300

    Add a sidebar

--- client/src/pages/admin/Layout.jsx
+++ client/src/pages/admin/Layout.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { assets } from "../../assets/assets";
 import { Outlet, useNavigate } from "react-router-dom";
+import Sidebar from "../../components/admin/Sidebar";
 
 const Layout = () => {
   const navigate = useNavigate();
@@ -18,7 +19,9 @@ const Layout = () => {
         </button>
       </div>
       <div className="flex h-[calc(100vh-70px)]">
-        <div>sidebar</div>
+        <div>
+          <Sidebar />
+        </div>
         <Outlet />
       </div>
     </>
