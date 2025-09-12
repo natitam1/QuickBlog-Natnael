b615825208611ea378be9cce10d2b49caaaf1972
Natnael Tamiru <natnaeltamiru98@gmail.com>
2025-09-12 18:36:25 +0300

    Add a loader component

--- client/src/pages/Blog.jsx
+++ client/src/pages/Blog.jsx
@@ -4,6 +4,7 @@ import { assets, blog_data, comments_data } from "../assets/assets";
 import Navbar from "../components/Navbar";
 import Moment from "moment";
 import Footer from "../components/Footer";
+import Loader from "../components/Loader";
 
 const Blog = () => {
   const { id } = useParams();
@@ -120,7 +121,9 @@ const Blog = () => {
       <Footer />
     </div>
   ) : (
-    <div>Loading</div>
+    <div>
+      <Loader />
+    </div>
   );
 };
 
