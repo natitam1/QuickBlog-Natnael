142d69a60b2c64c29364833a571c8a85211cef2b
Natnael Tamiru <natnaeltamiru98@gmail.com>
2025-09-13 14:28:01 +0300

    Display the dashboard data

--- client/src/pages/admin/Dashboard.jsx
+++ client/src/pages/admin/Dashboard.jsx
@@ -1,5 +1,5 @@
-import React, { useState } from "react";
-import { dashboard_data } from "../../assets/assets";
+import React, { useEffect, useState } from "react";
+import { assets, dashboard_data } from "../../assets/assets";
 
 const Dashboard = () => {
   const [dashboardData, setDashboardData] = useState({
@@ -12,7 +12,43 @@ const Dashboard = () => {
   const fetchDashboard = async () => {
     setDashboardData(dashboard_data);
   };
-  return <div></div>;
+
+  useEffect(() => {
+    fetchDashboard();
+  }, []);
+  return (
+    <div className="flex-1 p-4 md:p-10 bg-blue-50/50">
+      <div className="flex flex-wrap gap-4">
+        <div className="flex items-center gap-4 bg-white p-4 min-w-58 rounded shadow cursor-pointer hover:scale-105 transition-all">
+          <img src={assets.dashboard_icon_1} alt="" />
+          <div>
+            <p className="text-xl font-semibold text-gray-600">
+              {dashboardData.blogs}
+            </p>
+            <p className="text-gray-400 font-light">Blogs</p>
+          </div>
+        </div>
+        <div className="flex items-center gap-4 bg-white p-4 min-w-58 rounded shadow cursor-pointer hover:scale-105 transition-all">
+          <img src={assets.dashboard_icon_2} alt="" />
+          <div>
+            <p className="text-xl font-semibold text-gray-600">
+              {dashboardData.comments}
+            </p>
+            <p className="text-gray-400 font-light">Comments</p>
+          </div>
+        </div>
+        <div className="flex items-center gap-4 bg-white p-4 min-w-58 rounded shadow cursor-pointer hover:scale-105 transition-all">
+          <img src={assets.dashboard_icon_3} alt="" />
+          <div>
+            <p className="text-xl font-semibold text-gray-600">
+              {dashboardData.drafts}
+            </p>
+            <p className="text-gray-400 font-light">Drafts</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Dashboard;
