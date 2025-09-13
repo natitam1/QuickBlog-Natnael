78ca83bdf345e805a2cbc1c15f28d1ce937d353d
Natnael Tamiru <natnaeltamiru98@gmail.com>
2025-09-13 16:09:42 +0300

    Add latest blogs table

--- client/src/pages/admin/Dashboard.jsx
+++ client/src/pages/admin/Dashboard.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { assets, dashboard_data } from "../../assets/assets";
+import BlogTableItem from "../../components/admin/BlogTableItem";
 
 const Dashboard = () => {
   const [dashboardData, setDashboardData] = useState({
@@ -45,6 +46,47 @@ const Dashboard = () => {
             </p>
             <p className="text-gray-400 font-light">Drafts</p>
           </div>
+        </div>
+      </div>
+      <div>
+        <div className="flex items-center gap-3 m-4 mt-6 text-gray-600">
+          <img src={assets.dashboard_icon_4} alt="" />
+          <p>Latest Blogs</p>
+        </div>
+        <div className="relative max-w-4xl overflow-x-auto shadow rounded-lg scrollbar-hide bg-white">
+          <table className="w-full text-sm text-gray-500">
+            <thead className="text-xs text-gray-600 text-left uppercase">
+              <tr>
+                <th scope="col" className="px-2 py-4 xl:px-6">
+                  #
+                </th>
+                <th scope="col" className="px-2 py-4">
+                  Blog Title
+                </th>
+                <th scope="col" className="px-2 max-sm:hidden py-4">
+                  Date
+                </th>
+                <th scope="col" className="px-2 max-sm:hidden py-4">
+                  Status
+                </th>
+                <th scope="col" className="px-2 py-4">
+                  Actions
+                </th>
+              </tr>
+            </thead>{" "}
+            <tbody>
+              {dashboardData.recentBlogs.map((blog, index) => {
+                return (
+                  <BlogTableItem
+                    key={blog._id}
+                    blog={blog}
+                    fetchBlogs={fetchDashboard}
+                    index={index + 1}
+                  />
+                );
+              })}
+            </tbody>
+          </table>
         </div>
       </div>
     </div>
