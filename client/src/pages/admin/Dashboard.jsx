e23cdfb241d231bed57a80541ae9e8aa26cedb68
Natnael Tamiru <natnaeltamiru98@gmail.com>
2025-09-13 13:48:05 +0300

    Store the dashboard data and fetch it

--- client/src/pages/admin/Dashboard.jsx
+++ client/src/pages/admin/Dashboard.jsx
@@ -1,7 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
+import { dashboard_data } from "../../assets/assets";
 
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  const [dashboardData, setDashboardData] = useState({
+    blogs: 0,
+    comments: 0,
+    drafts: 0,
+    recentBlogs: [],
+  });
+
+  const fetchDashboard = async () => {
+    setDashboardData(dashboard_data);
+  };
+  return <div></div>;
 };
 
 export default Dashboard;
