e10f83f4ad7902e1379efede201123a4b4561ef4
Natnael Tamiru <natnaeltamiru98@gmail.com>
2025-09-14 18:02:34 +0300

    Add state variables and set the logic behind it

--- client/src/pages/admin/Comments.jsx
+++ client/src/pages/admin/Comments.jsx
@@ -1,6 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
+import { comments_data } from "../../assets/assets";
 
 const Comments = () => {
+  const [comments, setComments] = useState([]);
+  const [filter, setFilter] = useState("Not Approved");
+  const fetchcomments = async () => {
+    setComments(comments_data);
+  };
   return <div>Comments</div>;
 };
 
