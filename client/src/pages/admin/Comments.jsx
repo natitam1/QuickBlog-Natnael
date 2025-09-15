467c2ab366ebd3d5c7e8d1fcd379fdbc72cda521
Natnael Tamiru <natnaeltamiru98@gmail.com>
2025-09-15 07:50:18 +0300

    Add header for comments

--- client/src/pages/admin/Comments.jsx
+++ client/src/pages/admin/Comments.jsx
@@ -11,7 +11,22 @@ const Comments = () => {
   useEffect(() => {
     fetchcomments();
   }, []);
-  return <div className="flex-1 pt-5 px-5"></div>;
+  return (
+    <div className="flex-1 pt-5 px-5 sm:pt-12 sm:pl-16 bg-blue-50/50">
+      <div className="flex justify-between items-center max-w-3xl">
+        <h1>Comments</h1>
+        <div className="flex gap-4">
+          <button
+            className={`shadow-custom-sm border rounded-full px-4 py-1 cursor-pointer text-xs ${
+              filter === "Approved" ? "text-primary" : "text-gray-700"
+            }`}
+          >
+            Approved
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Comments;
