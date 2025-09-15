c28e6e89dbcbb17b506d5167bb1aa25bc74fef17
Natnael Tamiru <natnaeltamiru98@gmail.com>
2025-09-15 23:37:45 +0300

    Handle submit and show thumbnail

--- client/src/pages/admin/AddBlog.jsx
+++ client/src/pages/admin/AddBlog.jsx
@@ -7,17 +7,30 @@ const AddBlog = () => {
   const [subtitle, setSubtitle] = useState("");
   const [category, setCategory] = useState("Startup");
   const [isPublished, setIsPublished] = useState("false");
+
+  const onSubmitHandler = async (e) => {
+    e.preventDefault();
+  };
   return (
-    <form className="flex-1 bg-blue-50/50 text-gray-600 h-full overflow-scroll">
+    <form
+      onSubmit={onSubmitHandler}
+      className="flex-1 bg-blue-50/50 text-gray-600 h-full overflow-scroll"
+    >
       <div className="bg-white w-full max-w-3xl p-4 md:p-10 sm:m-10 shadow rounded">
         <p>Upload thumbnail</p>
         <label htmlFor="image">
           <img
-            src={assets.upload_area}
-            className="mt-2 h-1/6 rounded cursor-pointer"
+            src={!image ? assets.upload_area : URL.createObjectURL(image)}
+            className="mt-2 h-16 rounded cursor-pointer"
             alt=""
           />
-          <input type="file" id="image" hidden required />
+          <input
+            onChange={(e) => setImage(e.target.files[0])}
+            type="file"
+            id="image"
+            hidden
+            required
+          />
         </label>
       </div>
     </form>
