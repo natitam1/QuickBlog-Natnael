e903cdc0ccc02db5a6f001b3ef1f2039236331dd
Natnael Tamiru <natnaeltamiru98@gmail.com>
2025-09-15 09:07:08 +0300

    Set up table data for comments

--- client/src/components/admin/CommentTableItem.jsx
+++ client/src/components/admin/CommentTableItem.jsx
@@ -1,9 +1,32 @@
 import React from "react";
+import { assets } from "../../assets/assets";
 
 const CommentTableItem = ({ comment, fetchComments }) => {
   const { blog, createdAt, _id } = comment;
   const BlogDate = new Date(createdAt);
-  return <div className="order-y border-gray-300"></div>;
+  return (
+    <div className="order-y border-gray-300">
+      <td className="px-6 py-4">
+        <b className="font-medium text-gray-600">Blog</b> : {blog.title}
+        <br />
+        <b className="font-medium text-gray-600">Name</b> : {comment.name}
+        <br />
+        <b className="font-medium text-gray-600">Comment</b> : {comment.content}
+      </td>
+      <td className="px-6 py-4 max-sm:hidden">
+        {BlogDate.toLocaleDateString()}
+      </td>
+      <td className="px-6 py-4">
+        <div>
+          {!comment.isApproved ? (
+            <img src={assets.tick_icon} alt="" />
+          ) : (
+            <p>Approved</p>
+          )}
+        </div>
+      </td>
+    </div>
+  );
 };
 
 export default CommentTableItem;
