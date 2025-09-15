7025bb2c6978a8bbbf825a4c37dfa96d74f03dcd
Natnael Tamiru <natnaeltamiru98@gmail.com>
2025-09-15 23:31:19 +0300

    Add state variables and thumbnail uploader

--- client/src/pages/admin/AddBlog.jsx
+++ client/src/pages/admin/AddBlog.jsx
@@ -1,7 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
+import { assets } from "../../assets/assets";
 
 const AddBlog = () => {
-  return <div>AddBlog</div>;
+  const [image, setImage] = useState(false);
+  const [title, setTitle] = useState("");
+  const [subtitle, setSubtitle] = useState("");
+  const [category, setCategory] = useState("Startup");
+  const [isPublished, setIsPublished] = useState("false");
+  return (
+    <form className="flex-1 bg-blue-50/50 text-gray-600 h-full overflow-scroll">
+      <div className="bg-white w-full max-w-3xl p-4 md:p-10 sm:m-10 shadow rounded">
+        <p>Upload thumbnail</p>
+        <label htmlFor="image">
+          <img
+            src={assets.upload_area}
+            className="mt-2 h-1/6 rounded cursor-pointer"
+            alt=""
+          />
+          <input type="file" id="image" hidden required />
+        </label>
+      </div>
+    </form>
+  );
 };
 
 export default AddBlog;
