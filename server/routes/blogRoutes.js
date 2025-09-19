7629f8938dd9500a541d1b4f9a9be657e7867f03
Natnael Tamiru <natnaeltamiru98@gmail.com>
2025-09-19 15:20:10 +0300

    Upload

--- server/routes/blogRoutes.js
+++ server/routes/blogRoutes.js
@@ -1,6 +1,7 @@
 import express from "express";
 import { addBlog } from "../controller/blogController.js";
+import upload from "../middleware/multer.js";
 
 const blogRouter = express.Router();
 
-blogRouter.post("/add", addBlog);
+blogRouter.post("/add", upload.single("image"), addBlog);
