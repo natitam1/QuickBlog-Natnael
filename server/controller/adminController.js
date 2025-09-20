bc576b1d83299825f04ea864030b93b9ff3ecad7
Natnael Tamiru <natnaeltamiru98@gmail.com>
2025-09-21 00:18:09 +0300

    Get all blogs for admin

--- server/controller/adminController.js
+++ server/controller/adminController.js
@@ -1,4 +1,5 @@
 import jwt from "jsonwebtoken";
+import Blog from "../models/Blog.js";
 
 export const adminLogin = async (req, res) => {
   try {
@@ -12,6 +13,15 @@ export const adminLogin = async (req, res) => {
 
     const token = jwt.sign({ email }, process.env.JWT_SECRET);
     res.json({ success: true, token });
+  } catch (error) {
+    res.json({ success: false, message: error.message });
+  }
+};
+
+export const getAllBlogsAdmin = async (req, res) => {
+  try {
+    const blogs = Blog.find({}).sort({ createdAt: -1 });
+    res.json({ success: true, blogs });
   } catch (error) {
     res.json({ success: false, message: error.message });
   }
