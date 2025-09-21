385fa745ae34ed9569da48a98262dc24a5c94a08
Natnael Tamiru <natnaeltamiru98@gmail.com>
2025-09-21 16:56:20 +0300

    Get all comments

--- server/controller/adminController.js
+++ server/controller/adminController.js
@@ -1,5 +1,6 @@
 import jwt from "jsonwebtoken";
 import Blog from "../models/Blog.js";
+import Comment from "../models/Comment.js";
 
 export const adminLogin = async (req, res) => {
   try {
@@ -22,6 +23,17 @@ export const getAllBlogsAdmin = async (req, res) => {
   try {
     const blogs = Blog.find({}).sort({ createdAt: -1 });
     res.json({ success: true, blogs });
+  } catch (error) {
+    res.json({ success: false, message: error.message });
+  }
+};
+
+export const getAllComments = async (req, res) => {
+  try {
+    const comments = await Comment.find({})
+      .populate("blog")
+      .sort({ createdAt: -1 });
+    res.json({ success: true, comments });
   } catch (error) {
     res.json({ success: false, message: error.message });
   }
