bccbde89f9bf3048e5555c7d248953626acbfc98
Natnael Tamiru <natnaeltamiru98@gmail.com>
2025-09-23 15:02:55 +0300

    Clear search

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,7 +1,20 @@
-import React from "react";
+import React, { useRef } from "react";
 import { assets } from "../assets/assets";
+import { useAppContext } from "../context/AppContext";
 
 const Header = () => {
+  const { input, setInput } = useAppContext();
+  const inputRef = useRef();
+
+  const onSubmitHandler = async (e) => {
+    e.preventDefault();
+    setInput(inputRef.current.value);
+  };
+
+  const onClear = () => {
+    setInput("");
+    inputRef.current.value = "";
+  };
   return (
     <div className="relative mx-8 sm:mx-16 xl:mx-24">
       <div className="text-center mt-20 mb-8">
@@ -18,9 +31,13 @@ const Header = () => {
           write without filters. Whether it's one word or a thousand, your story
           starts right here.
         </p>
-        <form className="flex justify-between max-w-lg max-sm:scale-75 mx-auto border border-gray-300 bg-white rounded overflow-hidden">
+        <form
+          onSubmit={onSubmitHandler}
+          className="flex justify-between max-w-lg max-sm:scale-75 mx-auto border border-gray-300 bg-white rounded overflow-hidden"
+        >
           <input
             type="text"
+            ref={inputRef}
             placeholder="Search for blogs"
             required
             className="w-full pl-4 outline-none"
@@ -32,6 +49,16 @@ const Header = () => {
             Search
           </button>
         </form>
+      </div>
+      <div className="text-center">
+        {input && (
+          <button
+            onClick={onClear}
+            className="border font-light text-xs py-1 px-3 rounded-sm shadow-custom-sm cursor-pointer"
+          >
+            Clear Search
+          </button>
+        )}
       </div>
       <img
         src={assets.gradientBackground}
